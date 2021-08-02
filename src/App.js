4524b91ce3141b9a7278bd5fd7af5bb5f1067707
Collin Draughon <draughoncollin@gmail.com>
2021-08-02 15:40:30 -0500

    revert to auth

--- src/App.js
+++ src/App.js
@@ -1,160 +1,90 @@
-import React, {useState, useCallback} from 'react';
-import {
-  Layout,
-  Page,
-  FooterHelp,
-  Card,
-  Link,
-  Button,
-  FormLayout,
-  TextField,
-  AccountConnection,
-  ChoiceList,
-  SettingToggle,
-} from '@shopify/polaris';
-import {ImportMinor} from '@shopify/polaris-icons';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API, Storage } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-export default function App() {
-  const [first, setFirst] = useState('');
-  const [last, setLast] = useState('');
-  const [email, setEmail] = useState('');
-  const [checkboxes, setCheckboxes] = useState([]);
-  const [connected, setConnected] = useState(false);
+const initialFormState = { name: '', description: '' }
 
-  const handleFirstChange = useCallback((value) => setFirst(value), []);
-  const handleLastChange = useCallback((value) => setLast(value), []);
-  const handleEmailChange = useCallback((value) => setEmail(value), []);
-  const handleCheckboxesChange = useCallback(
-    (value) => setCheckboxes(value),
-    [],
-  );
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
 
-  const toggleConnection = useCallback(() => {
-    setConnected(!connected);
-  }, [connected]);
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-  const breadcrumbs = [{content: 'Sample apps'}, {content: 'Create React App'}];
-  const primaryAction = {content: 'New product'};
-  const secondaryActions = [{content: 'Import', icon: ImportMinor}];
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
 
-  const choiceListItems = [
-    {label: 'I accept the Terms of Service', value: 'false'},
-    {label: 'I consent to receiving emails', value: 'false2'},
-  ];
-
-  const accountSectionDescription = connected
-    ? 'Disconnect your account from your Shopify store.'
-    : 'Connect your account to your Shopify store.';
-
-  const accountMarkup = connected ? (
-    <DisconnectAccount onAction={toggleConnection} />
-  ) : (
-    <ConnectAccount onAction={toggleConnection} />
-  );
-
-  return (
-    <Page
-      title="Polaris"
-      breadcrumbs={breadcrumbs}
-      primaryAction={primaryAction}
-      secondaryActions={secondaryActions}
-    >
-      <Layout>
-        <Layout.AnnotatedSection
-          title="Style"
-          description="Customize the style of your checkout"
-        >
-          <SettingToggle
-            action={{
-              content: 'Customize Checkout',
-            }}
-          >
-            Upload your store’s logo, change colors and fonts, and more.
-          </SettingToggle>
-        </Layout.AnnotatedSection>
-
-        <Layout.AnnotatedSection
-          title="Account"
-          description={accountSectionDescription}
-        >
-          {accountMarkup}
-        </Layout.AnnotatedSection>
-
-        <Layout.AnnotatedSection
-          title="Form"
-          description="A sample form using Polaris components."
-        >
-          <Card sectioned>
-            <FormLayout>
-              <FormLayout.Group>
-                <TextField
-                  value={first}
-                  label="First name"
-                  placeholder="Tom"
-                  onChange={handleFirstChange}
-                />
-                <TextField
-                  value={last}
-                  label="Last name"
-                  placeholder="Ford"
-                  onChange={handleLastChange}
-                />
-              </FormLayout.Group>
-
-              <TextField
-                value={email}
-                label="Email"
-                placeholder="example@email.com"
-                onChange={handleEmailChange}
-              />
-
-              <ChoiceList
-                allowMultiple
-                choices={choiceListItems}
-                selected={checkboxes}
-                onChange={handleCheckboxesChange}
-              />
-
-              <Button primary>Submit</Button>
-            </FormLayout>
-          </Card>
-        </Layout.AnnotatedSection>
-
-        <Layout.Section>
-          <FooterHelp>
-            For more details on Polaris, visit our{' '}
-            <Link url="https://polaris.shopify.com">style guide</Link>.
-          </FooterHelp>
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
-}
-
-function ConnectAccount({onAction}) {
-  return (
-    <AccountConnection
-      action={{content: 'Connect', onAction}}
-      details="No account connected"
-      termsOfService={
-        <p>
-          By clicking Connect, you are accepting Sample’s{' '}
-          <Link url="https://polaris.shopify.com">Terms and Conditions</Link>,
-          including a commission rate of 15% on sales.
-        </p>
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
       }
-    />
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+          {
+            notes.map(note => (
+              <div key={note.id || note.name}>
+                <h2>{note.name}</h2>
+                <p>{note.description}</p>
+                <button onClick={() => deleteNote(note)}>Delete note</button>
+                {
+                  note.image && <img src={note.image} style={{width: 400}} />
+                }
+              </div>
+            ))
+          }
+      </div>
+      <input type="file" onChange={onChange}/>
+      <AmplifySignOut />
+    </div>
   );
 }
 
-function DisconnectAccount({onAction}) {
-  return (
-    <AccountConnection
-      connected
-      action={{content: 'Disconnect', onAction}}
-      accountName="Tom Ford"
-      title={<Link url="http://google.com">Tom Ford</Link>}
-      details="Account id: d587647ae4"
-    />
-  );
-}
+export default withAuthenticator(App);
